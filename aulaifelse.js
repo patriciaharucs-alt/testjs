e2fa1117d9ac8455224aa80d92da95b444d56999
Patricia Cassiana <patricia.harucs@gmail.com>
2026-05-22 16:49:23 -0300

    Add files via upload

---
+++ aulaifelse.js
@@ -0,0 +1,26 @@
+const button = document.getElementById('oie-sim')
+const campoTexto = document.getElementById('palavra-dia')
+const areaMensagem = document.getElementById('mensagem-resultado')
+
+
+
+button.addEventListener("click", doSomething)
+
+function doSomething() {
+  const valorDigitado =campoTexto.value; areaMensagem.textContent = valorDigitado;
+
+  const valorsalvo = localStorage.getItem('textousuario');
+
+  if (valorsalvo) { areaMensagem.textContent =  valorsalvo;
+
+  }
+button.addEventListener("click", doSomething);
+
+function doSomething() 
+  const valorDigitado = campoTexto.value;
+
+  if (valorDigitado.trim() !== "") {
+    areaMensagem.textContent = valorDigitado;          
+    localStorage.setItem('textousuario', valorDigitado); 
+    campoTexto.value = "";                              
+  }
